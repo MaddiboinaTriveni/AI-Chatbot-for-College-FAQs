e7604a33ef9aabeb0017877a466690072599adb8
Maddiboina Triveni <trivenimaddiboina@gmail.com>
2026-05-30 16:03:08 +0530

    Create sacet.js

---
+++ src/data/sacet.js
@@ -0,0 +1,26 @@
+export const SACET = {
+  name: "SACET Engineering College",
+
+  admissions: "Admissions are based on EAMCET rankings.",
+location: "CHIRALA",
+  courses: [
+    "CSE",
+    "ECE",
+    "EEE",
+    "Mechanical",
+    "Civil"
+  ],
+
+  fees: "₹75,000 per year",
+  
+  placements: {
+    highest: "12 LPA",
+    average: "4 LPA"
+  },
+ Facaulty: " Good AND Supportive Facaulty",
+  hostel: "Separate boys hostel facilities available",
+  hostel: "Separate girls hostel facilities available",
+  contact: "+91-9876543210",
+  principal : "Dr.K.JAGADEESH BABU has received PhD in 2013 and M.Tech. in 2006 from JNTUH",
+  cse_hod : "Dr.P.Harini",
+};
