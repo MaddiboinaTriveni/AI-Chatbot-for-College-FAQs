2b609cf82bd528eba5a5e797714781db4a0c89e9
Maddiboina Triveni <trivenimaddiboina@gmail.com>
2026-05-30 15:53:25 +0530

    Create colleges.js

---
+++ src/data/colleges.js
@@ -0,0 +1,16 @@
+import { SACET } from './sacet.js';
+import { JNTU } from './jntu.js';
+import { VIT } from './vit.js';
+import { IIT} from './iit.js';
+export const COLLEGES = {
+
+  sacet: SACET,
+
+  jntu: JNTU,
+
+  vit: VIT,
+  iit: IIT
+
+};
+
+
