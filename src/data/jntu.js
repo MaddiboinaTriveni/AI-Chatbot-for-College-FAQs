a0e445b7b53fe9f1c9c1176c7805812afb947dfb
Maddiboina Triveni <trivenimaddiboina@gmail.com>
2026-05-30 16:04:37 +0530

    Create jntu.js

---
+++ src/data/jntu.js
@@ -0,0 +1,29 @@
+export const JNTU = {
+
+  name: "JNTU Kakinada",
+
+  admissions:
+    "Admissions through EAMCET and ECET.",
+
+  courses: [
+    "CSE",
+    "IT",
+    "Civil",
+    "ECE"
+  ],
+
+  fees:
+    "₹90,000 per year",
+
+  placements: {
+    highest: "18 LPA",
+    average: "6 LPA"
+  },
+
+  hostel:
+    "Hostel available for boys and girls",
+
+  contact:
+    "+91-9988776655"
+};
+
